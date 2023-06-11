a65573ac21064d08885c43aee0080bc50b56846d
Matthew Hazlett <hazlema@gmail.com>
2023-06-11 16:02:59 -0400

    Ignore Fix

--- jokes.js
+++ jokes.js
@@ -1,49 +1,49 @@
-import axios from 'axios';
-import chalk from 'chalk';
-import { Debug } from './debug.js'
+import axios from "axios";
+import chalk from "chalk";
+import { Debug } from "./debug.js";
 
 var Jokes = class {
     constructor() {
-        this.debug = new Debug()
+        this.debug = new Debug();
     }
 
     query = (args) => {
-        this.debug.log(`Server Query: https://v2.jokeapi.dev/${args}`)
-        
+        this.debug.log(`Server Query: https://v2.jokeapi.dev/${args}`);
+
         return new Promise((response, reject) => {
             axios
                 .get(`https://v2.jokeapi.dev/${args}`)
 
-                .then(data => {
-                    this.debug.log(`Success!`)
-                    response(data.data)
+                .then((data) => {
+                    this.debug.log(`Success!`);
+                    response(data.data);
                 })
 
-                .catch(err => {
-                    this.debug.log(`Fail!`)
-                    reject(new Error("A network error has occurred: ", err))
-                })
-        })
-    }
-    
+                .catch((err) => {
+                    this.debug.log(`Fail!`);
+                    reject(new Error("A network error has occurred: ", err));
+                });
+        });
+    };
+
     get = async (category) => {
-        this.debug.log(`Fetching joke from: ${category}`)
-        let data = await this.query(`joke/${category}`)
+        this.debug.log(`Fetching joke from: ${category}`);
+        let data = await this.query(`joke/${category}`);
 
         if (data.joke) {
-            console.log(chalk.blueBright(data.joke))
+            console.log(chalk.blueBright(data.joke));
         } else {
-            console.log(chalk.blueBright(data.setup))
-            console.log(chalk.yellowBright(data.delivery))
+            console.log(chalk.blueBright(data.setup));
+            console.log(chalk.yellowBright(data.delivery));
         }
-    }
+    };
 
     list = async () => {
-        this.debug.log(`Fetching list of categories`)
-        let data = await this.query('categories')
+        this.debug.log(`Fetching list of categories`);
+        let data = await this.query("categories");
 
-        console.log(data.categories.join(', '))
-    }  
-}
+        console.log(data.categories.join(", "));
+    };
+};
 
-export { Jokes }
+export { Jokes };
