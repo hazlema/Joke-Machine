0585c57bbd4f3294e0c72277f9e2a73ef38e1ab8
Matthew Hazlett <hazlema@gmail.com>
2023-06-11 20:03:16 -0400

    Version 1

--- jokes.js
+++ jokes.js
@@ -3,99 +3,114 @@ import chalk from "chalk";
 import { Debug } from "./debug.js";
 
 var Jokes = class {
-    constructor() {
-        this.debug = new Debug();
-    }
+	constructor() {
+		this.debug = new Debug();
+	}
 
-    query = (args) => {
-        this.debug.log(`Server Query: https://v2.jokeapi.dev/${args}`);
+	/**
+	 * Preform a query on the API
+	 * @param {string} args Arguments for the query
+	 * @returns object
+	 */
+	query = (args) => {
+		this.debug.log(`Server Query: https://v2.jokeapi.dev/${args}`);
 
-        return new Promise((response, reject) => {
-            axios
-                .get(`https://v2.jokeapi.dev/${args}`)
+		return new Promise((response, reject) => {
+			axios
+				.get(`https://v2.jokeapi.dev/${args}`)
 
-                .then((data) => {
-                    this.debug.log(`Success!`);
-                    response(data.data);
-                })
+				.then((data) => {
+					this.debug.log(`Success!`);
+					response(data.data);
+				})
 
-                .catch((err) => {
-                    this.debug.log(`Fail!`);
-                    reject(new Error("A network error has occurred: ", err));
-                });
-        });
-    };
+				.catch((err) => {
+					this.debug.log(`Fail!`);
+					reject(new Error("A network error has occurred: ", err));
+				});
+		});
+	};
 
-    isCategory = (isValid) => {
-        this.debug.log(`Fetching list of categories`);
+	/**
+	 * Tests if this is a category
+	 * @param {string} category The category name to test
+	 * @returns {bool}
+	 */
+	isCategory = (category) => {
+		this.debug.log(`Fetching list of categories`);
 
-        return new Promise((resolve, reject) => {
-            this.query("categories")
-                .then((data) => {
-                    const categories = data.categories;
+		return new Promise((resolve, reject) => {
+			this.query("categories")
+				.then((data) => {
+					const categories = data.categories;
+					this.debug.log(`Categories: ` + categories.join(", "));
 
-                    this.debug.log(`Categories: ` + categories.join(", "));
+					for (let test of categories) {
+						this.debug.log(
+							`Compare ${category.toLowerCase()} and ${test.toLowerCase()}`
+						);
+						if (category.toLowerCase() === test.toLowerCase()) {
+							resolve(true);
+							return;
+						}
+					}
+					resolve(false);
+				})
+				.catch((error) => reject(error));
+		});
+	};
 
-                    for (let category of categories) {
-                        this.debug.log(
-                            `Compare ${isValid.toLowerCase()} and ${category.toLowerCase()}`
-                        );
-                        if (isValid.toLowerCase() === category.toLowerCase()) {
-                            resolve(true);
-                            return; // Exit the loop if a match is found
-                        }
-                    }
-                    resolve(false); // Resolve with false if no match is found
-                })
-                .catch((error) => {
-                    reject(error); // Reject the promise if there's an error
-                });
-        });
-    };
+	/**
+	 * Get a joke from the API
+	 * @param {string} category, The category that should be used
+	 * @param {bool} skipCheck, If we should skip the category check
+	 * @returns {bool}
+	 */
+	get = async (category, skipCheck = false) => {
+		let isValid = skipCheck ? true : await this.isCategory(category);
 
-    get = async (category, skipCheck = false) => {
-        let isValid = false;
+		if (isValid) {
+			this.debug.log(`Fetching joke, category: ${category}`);
+			let data = await this.query(`joke/${category}`);
 
-        if (skipCheck) {
-            isValid = true;
-        } else {
-            isValid = await this.isCategory(category);
-        }
+			if (data.joke) {
+				console.log(chalk.blueBright(data.joke));
+			} else {
+				console.log(chalk.blueBright(data.setup));
+				console.log(chalk.yellowBright(data.delivery));
+			}
+		} else {
+			this.debug.log(`Invalid Category`);
+			return false;
+		}
 
-        if (isValid) {
-            this.debug.log(`Fetching joke, category: ${category}`);
-            let data = await this.query(`joke/${category}`);
+		return true;
+	};
 
-            if (data.joke) {
-                console.log(chalk.blueBright(data.joke));
-            } else {
-                console.log(chalk.blueBright(data.setup));
-                console.log(chalk.yellowBright(data.delivery));
-            }
-        } else {
-            this.debug.log(`Invalid Category`);
-            return false;
-        }
-    };
+	/**
+	 * List the categories
+	 */
+	list = async () => {
+		this.debug.log(`Fetching list of categories`);
+		let data = await this.query("categories");
 
-    list = async () => {
-        this.debug.log(`Fetching list of categories`);
-        let data = await this.query("categories");
+		console.log(data.categories.join(", "));
+	};
 
-        console.log(data.categories.join(", "));
-    };
+	/**
+	 * Get a random category then query the data source
+	 */
+	rand = async () => {
+		this.debug.log(`Fetching list of categories`);
+		let data = await this.query("categories");
+		const categories = data.categories.slice(1);
 
-    rand = async () => {
-        this.debug.log(`Fetching list of categories`);
-        let data = await this.query("categories");
-        const categories = data.categories.slice(1);
+		this.debug.log(`Categories: ${categories.join(", ")}`);
 
-        this.debug.log(`Categories: ${categories.join(", ")}`);
-        this.get(
-            categories[Math.floor(Math.random() * categories.length)],
-            true
-        );
-    };
+		let category =
+			categories[Math.floor(Math.random() * categories.length)];
+		this.get(category, true); // Skip category check, we know this is valid
+	};
 };
 
 export { Jokes };
